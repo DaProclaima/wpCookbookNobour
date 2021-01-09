d98518e1cc30a4cb0997050900152adad8dadb15
daproclaima <sn.itpro@gmail.com>
2021-01-09 03:58:15 +0100

    feat(01-plugin-api): add plugin explains use of apply_filters() and add_filter() with wordpress hooks

---
+++ app/public/wp-content/plugins/01-plugin-api/01-plugin-api.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Plugin Name: Plugin API - Basic hook examples.
+ * Plugin URI: https://example.com/plugins/01-plugin-api/
+ * Description: This registers a few basic hooks, just to see how hooks work.
+ * Version: 1.0
+ * Requires at least: 5.2
+ * Requires PHP: 8.0.0
+ * Author: Vincent Dubroeucq
+ * Author URI: https://vincentdubroeucq.com/
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: 01-plugin-api
+ * Domain Path: languages/
+ */
+
+//
+// execute die() if someone tries to directly access the file without passing through WordPress
+defined( 'ABSPATH' ) || die();
+
+/*
+ * Functions declared outside of a PHP class are available globally.
+   This means that they can come into conflict with functions of WordPress or other extensions
+   (try to declare a function called the_title to see!).
+   To avoid any name conflict, you must imperatively prefix your functions.
+   By convention, the prefix should be the slug of your extension. So here, our function should normally
+   be called 01_plugin_api_title( ).
+   But, starting a function with a number is invalid, so to make it simpler I will use the prefix
+   wpcookbook_ .
+*/
+//add_filter( string $tag, callable $function_to_add, int $priority = 10, int $accepted_args = 2 )
+//add_filter( 'the_title', 'wpcookbook_title' );
+add_filter( 'the_title', 'wpcookbook_title', 10, 2 );
+
+/*
+ * Somewhere the system executes
+ * return apply_filters( 'the_title', $title, $id );
+ *  whose syntax is apply_filters( string $tag, mixed $value [, mixed $...] )
+ * */
+/**
+ * Changes the post and page titles to 'TOTO'
+ *
+ * @param string $title The title of the post
+ * @param int $id The post id
+ * @return string $title
+ */
+function wpcookbook_title( $title, $id ){
+    $title = 'TOTO ' . $id;
+    return $title;
+}

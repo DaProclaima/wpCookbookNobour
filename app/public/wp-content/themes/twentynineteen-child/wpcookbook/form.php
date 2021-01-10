3ecf235d8ac4b4cb6e6a8e8ae15048d242022c66
daproclaima <sn.itpro@gmail.com>
2021-01-10 17:56:16 +0100

    feat(06-adding-hooks): add contact form

---
+++ app/public/wp-content/themes/twentynineteen-child/wpcookbook/form.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Our contact form template.
+ */
+defined( 'ABSPATH' ) || die();
+?>
+
+<p><?php echo $strings['dummy_text']; ?></p>
+<h2><?php esc_html_e( 'I am the child theme’s template', 'twentynineteenchild'
+	); ?></h2>
+<form id="wpcookbook-form" action="" method="POST">
+	<form id="wpcookbook-form" action="" method="POST">
+		<p>
+			<label for="name" style="display: block;"><?php esc_html_e(
+					'Your name', '06-adding-hooks' );?></label>
+			<input type="text" id="name" name="name" required />
+		</p>
+		<p>
+			<label for="email" style="display: block;"><?php esc_html_e(
+					'Your email', '06-adding-hooks' );?></label>
+			<input type="email" id="email" name="email" required />
+		</p>
+		<p>
+			<label for="message" style="display: block;"><?php
+				esc_html_e( 'Your message', '06-adding-hooks' );?></label>
+			<textarea id="message" placeholder="<?php esc_attr_e( 'Type
+something', '06-adding-hooks'); ?>"></textarea>
+		</p>
+		<p>
+			<input type="submit" value="<?php esc_attr_e( 'Send 
+message', '06-adding-hooks'); ?>">
+		</p>
+	</form>

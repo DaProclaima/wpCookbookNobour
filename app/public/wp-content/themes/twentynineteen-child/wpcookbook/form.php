2592111420095778475a5160ccda54a25435f9cb
daproclaima <sn.itpro@gmail.com>
2021-01-10 22:19:09 +0100

    feat(06-adding-hook): finish plugin and add form template in twentynineteen-child

--- app/public/wp-content/themes/twentynineteen-child/wpcookbook/form.php
+++ app/public/wp-content/themes/twentynineteen-child/wpcookbook/form.php
@@ -5,29 +5,30 @@
 defined( 'ABSPATH' ) || die();
 ?>
 
-<p><?php echo $strings['dummy_text']; ?></p>
-<h2><?php esc_html_e( 'I am the child theme’s template', 'twentynineteenchild'
-	); ?></h2>
-<form id="wpcookbook-form" action="" method="POST">
-	<form id="wpcookbook-form" action="" method="POST">
-		<p>
-			<label for="name" style="display: block;"><?php esc_html_e(
-					'Your name', '06-adding-hooks' );?></label>
-			<input type="text" id="name" name="name" required />
-		</p>
-		<p>
-			<label for="email" style="display: block;"><?php esc_html_e(
-					'Your email', '06-adding-hooks' );?></label>
-			<input type="email" id="email" name="email" required />
-		</p>
-		<p>
-			<label for="message" style="display: block;"><?php
-				esc_html_e( 'Your message', '06-adding-hooks' );?></label>
-			<textarea id="message" placeholder="<?php esc_attr_e( 'Type
-something', '06-adding-hooks'); ?>"></textarea>
-		</p>
-		<p>
-			<input type="submit" value="<?php esc_attr_e( 'Send 
-message', '06-adding-hooks'); ?>">
-		</p>
-	</form>
+<?php do_action( 'wpcookbook_before_form', $strings ); ?>
+    <form id="wpcookbook-form" action="" method="POST">
+		<?php do_action( 'wpcookbook_before_fields', $strings ); ?>
+        <p>
+            <label for="name" style="display: block;"><?php echo esc_html( $strings['name']['label'] ); ?></label>
+            <input type="text" id="name" name="name"
+                   placeholder="<?php echo esc_attr( $strings['name']['placeholder'] ); ?>" required
+            />
+        </p>
+        <p>
+            <label for="email" style="display: block;"><?php echo esc_html( $strings['email']['label'] ); ?></label>
+            <input type="email" id="email" name="email"
+                   placeholder="<?php echo esc_attr( $strings['email']['placeholder'] ); ?>" required
+            />
+        </p>
+        <p>
+            <label for="message" style="display: block;"><?php echo esc_html( $strings['message']['label'] ); ?></label>
+            <textarea id="message" placeholder="<?php echo esc_attr(
+				$strings['message']['placeholder'] ); ?>"
+            ></textarea>
+        </p>
+		<?php do_action( 'wpcookbook_after_fields', $strings ); ?>
+        <p>
+            <input type="submit" value="<?php esc_attr_e( 'Send message', '06-adding-hooks' ); ?>">
+        </p>
+    </form>
+<?php do_action( 'wpcookbook_after_form', $strings ); ?>
